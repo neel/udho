b3b9c782bf71799bdd5a2902d5293f577bae7461
Neel Basu <neel.basu.z@gmail.com>
2020-06-01 21:15:15 +0530

    - progress

---
+++ includes/udho/configuration.h
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2020, Neel Basu <neel.basu.z@gmail.com>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY Neel Basu <neel.basu.z@gmail.com> ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Neel Basu <neel.basu.z@gmail.com> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef CONFIGURATION_H
+#define CONFIGURATION_H
+
+#include <string>
+
+/**
+ * typedef udho::configuration<udho::config::server, udho::config::router, udho::config::logging, udho::config::view> configuration;
+ * configuration config;
+ * config[udho::configs::document_root] = "/path/to/document/root";
+ */
+
+namespace udho{
+/**
+ * @todo write docs
+ */
+template <typename... T>
+class configuration{
+    
+};
+
+template <typename T>
+struct config: T{
+    typedef config<T> self_type;    
+    
+    template <typename K>
+    struct proxy{
+        typedef K key_type;
+        typedef proxy<K> proxy_type;
+        typedef decltype(self_type().get(K())) value_type;
+        
+        self_type& _self;
+        
+        proxy(self_type& self): _self(self){}
+        value_type value() const{
+            return _self.get(K());
+        }
+        operator value_type() const{
+            return value();
+        }
+        template <typename V>
+        proxy_type& operator=(V value){
+            _self.set(K(), value);
+            return *this;
+        }
+    };
+        
+    template <typename K>
+    auto operator[](const K& /*key*/) const{
+        return T::get(K());
+    }
+    template <typename K>
+    proxy<K> operator[](const K& /*key*/){
+        return proxy<K>(*this);
+    }
+};
+
+// template <typename K, typename... T>
+// std::ostream& operator<<(std::ostream& stream, const typename config<T...>::proxy<K>& p){
+//     stream << p.value();
+//     return stream;
+// }
+
+
+namespace configs{
+template <typename T = void>
+struct server_{
+    const static struct port_t{} port;
+    const static struct document_root_t{} document_root;
+    const static struct template_root_t{} template_root;
+    
+    unsigned _port;
+    std::string _document_root;
+    std::string _template_root;
+    
+    inline void set(port_t, unsigned v){_port = v;}
+    inline unsigned get(port_t) const{return _port;}
+    
+    inline void set(document_root_t, std::string v){_document_root = v;}
+    inline std::string get(document_root_t) const{return _document_root;}
+    
+    inline void set(template_root_t, std::string v){_template_root = v;}
+    inline std::string get(template_root_t) const{return _template_root;}
+};
+
+template <typename T> const typename server_<T>::port_t server_<T>::port;
+template <typename T> const typename server_<T>::document_root_t server_<T>::document_root;
+template <typename T> const typename server_<T>::template_root_t server_<T>::template_root;
+
+typedef server_<> server;
+
+}
+
+
+
+}
+
+#endif // CONFIGURATION_H
--- tests/sandbox.cpp
+++ tests/sandbox.cpp
@@ -4,175 +4,18 @@
 #include <udho/scope.h>
 #include <udho/access.h>
 #include <udho/parser.h>
-#include <boost/lexical_cast/try_lexical_convert.hpp>
-
-struct book: udho::prepare<book>{
-    std::string title;
-    unsigned    year;
-    std::vector<std::string>  authors;
-       
-    template <typename DictT>
-    auto dict(DictT assoc) const{
-        return assoc | var("title",   &book::title)
-                     | var("authors", &book::authors)
-                     | var("year",    &book::year);
-    }
-};
-
-struct publisher: udho::prepare<publisher>{
-    std::string name;
-    std::string address;
-    unsigned    year;
-       
-    template <typename DictT>
-    auto dict(DictT assoc) const{
-        return assoc | var("label",   &publisher::name)
-                     | var("since",   &publisher::year)
-                     | var("address", &publisher::address);
-    }
-};
-
-struct student: udho::prepare<student>{
-    unsigned int roll;
-    std::string  first;
-    std::string  last;
-    std::vector<book> publications;
-    std::map<std::string, double> marks_obtained;
-    
-    std::string name() const{
-        return first + " " + last;
-    }
-    bool qualified() const{
-        return true;
-    }
-    template <typename DictT>
-    auto dict(DictT assoc) const{
-        return assoc | var("roll",     &student::roll)
-                     | var("first",    &student::first)
-                     | var("last",     &student::last)
-                     | var("books",    &student::publications)
-                     | var("marks",    &student::marks_obtained)
-                     | fn("name",      &student::name)
-                     | fn("qualified", &student::qualified);
-    }
-};
-
-struct lazy: udho::prepare<lazy>{
-    bool populated;
-    mutable bool _value_called;
-    
-    lazy(): populated(false), _value_called(false){}
-    double value() const{
-        _value_called = true;
-        return populated;
-    }
-    
-    template <typename DictT>
-    auto dict(DictT assoc) const{
-        return assoc | var("populated",  &lazy::populated)
-                     | fn ("value",      &lazy::value);
-    }
-};
+#include <udho/configuration.h>
 
 int main(){
-    book b1;
-    b1.title = "Book1 Title";
-    b1.year  = 2020;
-    b1.authors.push_back("Sunanda Bose");
-    b1.authors.push_back("Neel Bose");
-    b1.authors.push_back("Nandini Mukherjee");
+    udho::config<udho::configs::server> conf;
     
-    book b2;
-    b2.title = "Book2 Title";
-    b2.year  = 2018;
-    b2.authors.push_back("Sunanda Bose");
-    b2.authors.push_back("Neel Bose");
-    b2.authors.push_back("Nandini Mukherjee");
+    conf[udho::configs::server::port] =  9198;
+    conf[udho::configs::server::document_root] = "/path/to/document/root";
     
-    student neel;
-    neel.roll  = 2;
-    neel.first = "Neel";
-    neel.last  = "Bose";
+    std::string value = conf[udho::configs::server::document_root];
     
-    neel.publications.push_back(b1);
-    neel.publications.push_back(b2);
-    neel.marks_obtained["chemistry"] = -10.42;
-    
-    udho::prepared<student> prepared(neel);
-    
-    std::vector<std::string> keys = prepared.keys("books:0.authors");
-
-    udho::lookup_table<udho::prepared<student>> table = udho::scope(prepared);
-    
-    table.add("papers", "books", 0);
-    table.add("thesis", "papers:0", 1);
-    table.clear(1);
-        
-    auto expr = udho::view::expression(table);
-    
-    lazy lobj;
-    lobj.populated = false;
-    auto plobj = udho::data(lobj);
-    auto tlobj = udho::scope(plobj);
-    auto elobj = udho::view::expression(tlobj);
-    elobj.evaluate<double>("populated && not(value)");
-    std::cout << "_value_called " << lobj._value_called << std::endl;
-    
-    std::string xml_template = R"TEMPLATE(
-            <div class="foo">
-                <span class="name">
-                    Hi! <udho:text name="name" />
-                </span>
-                <udho:block>
-                    <article class="thesis">
-                        <udho:var name="thesis" value="books:0" />
-                        <label class="year">
-                            <udho:text name="thesis.year" />
-                        </label>
-                    </article>
-                </udho:block>
-                <div class="publications" udho:if="count(books) > 1">
-                    <udho:for value="book" key="id" in="books">
-                        <div class="title" udho:target:title="book.title">
-                            <udho:text name="book.title" />
-                        </div>
-                    </udho:for>
-                </div>
-                <udho:if test="not(count(books) > 1)">
-                    <div class="freshers">
-                        Not much publications
-                    </div>
-                </udho:if>
-            </div>
-    )TEMPLATE";
-    
-    udho::view::parser<udho::lookup_table<udho::prepared<student>>> processor = udho::view::processor(table);
-    std::string output = processor.process(xml_template);
-    std::cout << output << std::endl;
-    
-    publisher pub;
-    pub.name    = "Some Publisher";
-    pub.address = "Somewhere";
-    pub.year    = 1987;
-
-    
-    auto package = (udho::data(neel) | udho::data(b1) | udho::data(pub));
-//     auto pkg_expr = udho::view::expression(package);
-    std::cout << package["name"] << std::endl;
-    auto scoped_pkg = udho::scope(package);
-    std::cout << scoped_pkg["name"] << std::endl;
-    auto scoped_pkg2 = udho::scope(udho::data(neel) | udho::data(b1) | udho::data(pub));
-    auto package2 = udho::data(neel, b1, pub);
-    std::cout << package2["name"] << std::endl;
-//     std::cout << package["title"] << std::endl;
-//     std::cout << package["label"] << std::endl;
-//     std::cout << package["year"] << std::endl;
-//     std::cout << package["since"] << std::endl;
-//     std::cout << pkg_expr.evaluate<int>("since + year") << std::endl;
-    
+//     std::cout << conf[udho::configs::server::document_root] << std::endl;
     
     return 0;
-    
-    
 }
 
