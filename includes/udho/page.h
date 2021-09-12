c83cb77b7dc2e6b5293c9aac8bfff57273b02a7a
Neel Basu <neel.basu.z@gmail.com>
2021-09-13 03:08:19 +0530

    - documented exception

--- includes/udho/page.h
+++ includes/udho/page.h
@@ -38,6 +38,13 @@ namespace exceptions{
         http_error(boost::beast::http::status status, const std::string& message = "");
         void add_header(boost::beast::http::field key, const std::string& value);
         void redirect(const std::string& url);
+        /**
+         * @brief Given an HTTP Request, Generates HTTP Response for the exception
+         * 
+         * @tparam T 
+         * @param request The HTTP Request
+         * @return boost::beast::http::response<boost::beast::http::string_body> 
+         */
         template <typename T>
         boost::beast::http::response<boost::beast::http::string_body> response(const boost::beast::http::request<T>& request) const{
             boost::beast::http::response<boost::beast::http::string_body> res{_status, request.version()};
@@ -51,10 +58,28 @@ namespace exceptions{
             res.prepare_payload();
             return res;
         }
+        /**
+         * @brief Given a udho request context generates HTTP response with the HTTP request inside the context
+         * 
+         * @tparam AuxT 
+         * @tparam U 
+         * @tparam V 
+         * @param ctx The udho Request Context
+         * @return boost::beast::http::response<boost::beast::http::string_body> 
+         */
         template <typename AuxT, typename U, typename V>
         boost::beast::http::response<boost::beast::http::string_body> response(const udho::context<AuxT, U, V>& ctx) const{
             return response(ctx.request());
         }
+        /**
+         * @brief Given an HTTP request and the URL Router Generates HTTP response 
+         * 
+         * @tparam T 
+         * @tparam RouterT 
+         * @param request HTTP Request
+         * @param router The URL Router
+         * @return boost::beast::http::response<boost::beast::http::string_body> 
+         */
         template <typename T, typename RouterT>
         boost::beast::http::response<boost::beast::http::string_body> response(const boost::beast::http::request<T>& request, RouterT& router) const{
             boost::beast::http::response<boost::beast::http::string_body> res{_status, request.version()};
@@ -68,17 +93,53 @@ namespace exceptions{
             res.prepare_payload();
             return res;
         }
+        /**
+         * @brief Given a udho request context and the URL Router Generates HTTP response using the HTTP request inside the context
+         * 
+         * @tparam AuxT 
+         * @tparam U 
+         * @tparam V 
+         * @tparam RouterT 
+         * @param ctx udho request context
+         * @param router The URL Router
+         * @return boost::beast::http::response<boost::beast::http::string_body> 
+         */
         template <typename AuxT, typename U, typename V, typename RouterT>
         boost::beast::http::response<boost::beast::http::string_body> response(const udho::context<AuxT, U, V>& ctx, RouterT& router) const{
             return response(ctx.request(), router);
         }
-        virtual std::string page(const std::string& target, std::string content="") const;
+        /**
+         * @brief Renders the HTML string using the target and the content
+         * 
+         * @param target 
+         * @param content 
+         * @return std::string 
+         */
+        std::string page(const std::string& target, std::string content="") const;
+        /**
+         * @brief Generates the HTML string with the summary of the URL router.
+         * 
+         * @tparam RouterT 
+         * @param target 
+         * @param router 
+         * @return std::string 
+         */
         template <typename RouterT>
         std::string page(const std::string& target, RouterT& router) const{
             std::string buffer = internal::html_summary(router);
             return page(target, buffer);
         }
+        /**
+         * @brief Returns the exception as plain text
+         * 
+         * @return const char* 
+         */
         virtual const char* what() const noexcept;
+        /**
+         * @brief HTTP Status assoviated with the exception
+         * 
+         * @return boost::beast::http::status 
+         */
         boost::beast::http::status result() const;
     };
     http_error http_redirection(const std::string& location, boost::beast::http::status status = boost::beast::http::status::temporary_redirect);
