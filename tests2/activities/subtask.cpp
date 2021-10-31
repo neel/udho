09c2ca957d30538b5057a30184051b0bb108b347
Neel Basu <neel.basu.z@gmail.com>
2021-11-01 01:44:44 +0530

    - unit tests

--- tests2/activities/subtask.cpp
+++ tests2/activities/subtask.cpp
@@ -157,39 +157,41 @@ TEST_CASE("subtask basic", "[activities]") {
     group.join_all();
     REQUIRE(test_run);
 
-    REQUIRE(a0->value() == 100*2+1); // 201
-    REQUIRE(a1->value() == 102*2+1); // 205
-    REQUIRE(a2->value() == 104*2+1); // 209
-    REQUIRE(a3->value() == 106*2+1); // 213
-    REQUIRE(a4->value() == 108*2+1); // 217
+    THEN("all subtasks are executed in proper sequence") {
+        REQUIRE(a0->value() == 100*2+1); // 201
+        REQUIRE(a1->value() == 102*2+1); // 205
+        REQUIRE(a2->value() == 104*2+1); // 209
+        REQUIRE(a3->value() == 106*2+1); // 213
+        REQUIRE(a4->value() == 108*2+1); // 217
+    }
 
-    REQUIRE(a0->time() <= a1->time());
-    REQUIRE(a0->time() <= a2->time());
-    REQUIRE(a1->time() <= a3->time());
-    REQUIRE(a3->time() <= a3->time());
-    REQUIRE(a3->time() <= a4->time());
-
-    std::cout << "a0" << " ";
-    std::copy(a0->begin(), a0->end(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout << std::endl;
-
-    std::cout << "a1" << " ";
-    std::copy(a1->begin(), a1->end(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout << std::endl;
-
-    std::cout << "a2" << " ";
-    std::copy(a2->begin(), a2->end(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout << std::endl;
-
-    std::cout << "a3" << " ";
-    std::copy(a3->begin(), a3->end(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout << std::endl;
-
-    std::cout << "a4" << " ";
-    std::copy(a4->begin(), a4->end(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout << std::endl;
+    THEN("all subtasks are executed after all dependent subtasks are done") {
+        REQUIRE(a0->time() <= a1->time());
+        REQUIRE(a0->time() <= a2->time());
+        REQUIRE(a1->time() <= a3->time());
+        REQUIRE(a3->time() <= a3->time());
+        REQUIRE(a3->time() <= a4->time());
+    }
 
     THEN("all subtasks can access its previous subtasks data through appropriate accessor") {
-        
+        std::cout << "a0" << " ";
+        std::copy(a0->begin(), a0->end(), std::ostream_iterator<int>(std::cout, " "));
+        std::cout << std::endl;
+
+        std::cout << "a1" << " ";
+        std::copy(a1->begin(), a1->end(), std::ostream_iterator<int>(std::cout, " "));
+        std::cout << std::endl;
+
+        std::cout << "a2" << " ";
+        std::copy(a2->begin(), a2->end(), std::ostream_iterator<int>(std::cout, " "));
+        std::cout << std::endl;
+
+        std::cout << "a3" << " ";
+        std::copy(a3->begin(), a3->end(), std::ostream_iterator<int>(std::cout, " "));
+        std::cout << std::endl;
+
+        std::cout << "a4" << " ";
+        std::copy(a4->begin(), a4->end(), std::ostream_iterator<int>(std::cout, " "));
+        std::cout << std::endl;
     }
 }
