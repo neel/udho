ecfd5ef4282abd1d788ea97121d89a31f3e998b2
neel.basu <neel.basu.z@gmail.com>
2023-06-18 13:36:36 +0200

    - both join and model using same schema

--- tests2/db/pg/join.cpp
+++ tests2/db/pg/join.cpp
@@ -12,11 +12,10 @@
 #include <udho/db/pg/schema/field.h>
 #include <udho/db/pg/schema/relation.h>
 #include <udho/db/pg/crud/join.h>
-
+#include "schema.h"
+#include "common.h"
 #include <udho/db/pg/generators/select.h>
 #include <udho/db/pg/generators/from.h>
-
-
 
 #include <boost/algorithm/string/replace.hpp>
 #include <boost/algorithm/string/erase.hpp>
@@ -25,104 +24,6 @@
 using namespace udho::db;
 using namespace ozo::literals;
 using namespace boost::hana::literals;
-
-// { relations
-
-namespace students{
-
-PG_ELEMENT(id,       std::int64_t, pg::constraints::primary);
-PG_ELEMENT(name,     std::string);
-PG_ELEMENT(marks,    std::int64_t);
-
-PG_ELEMENT(projects_associated, std::int64_t);
-PG_ELEMENT(articles_published, std::int64_t);
-
-struct table: pg::relation<table, id, name, marks>{
-    PG_NAME(students)
-    using readonly = pg::readonly<id>;
-};
-
-}
-
-namespace projects{
-
-PG_ELEMENT(id,     std::int64_t, pg::constraints::primary);
-PG_ELEMENT(title,  std::string,  pg::constraints::unique);
-PG_ELEMENT(admin,  std::int64_t, pg::constraints::references<students::table::column<students::id>>::restrict);
-
-struct table: pg::relation<table, id, title, admin>{
-    PG_NAME(projects)
-    using readonly = pg::readonly<id>;
-};
-
-}
-
-namespace articles{
-
-PG_ELEMENT(id,      std::int64_t, pg::constraints::primary);
-PG_ELEMENT(title,   std::string,  pg::constraints::unique);
-PG_ELEMENT(author,  std::int64_t, pg::constraints::references<students::table::column<students::id>>::restrict);
-PG_ELEMENT(project, std::int64_t, pg::constraints::references<projects::table::column<projects::id>>::restrict);
-
-struct table: pg::relation<table, id, title, author, project>{
-    PG_NAME(articles)
-    using readonly = pg::readonly<id>;
-};
-
-}
-
-namespace memberships{
-
-PG_ELEMENT(id,      std::int64_t, pg::constraints::primary);
-PG_ELEMENT(student, std::int64_t, pg::constraints::references<students::table::column<students::id>>::restrict);
-PG_ELEMENT(project, std::int64_t, pg::constraints::references<projects::table::column<projects::id>>::restrict);
-
-struct table: pg::relation<table, id, student, project>{
-    PG_NAME(memberships)
-    using readonly = pg::readonly<id>;
-};
-
-}
-
-// } relations
-
-struct sql{
-    std::string _query;
-
-    sql() = default;
-    explicit sql(const char* q): _query(q) {}
-    sql(const sql&) = default;
-
-    std::string query() const { 
-        std::string q = boost::algorithm::trim_copy(_query);
-        return std::regex_replace(q, std::regex("\\s+"), " ");
-    }
-
-    friend bool operator==(const sql& lhs, const sql& rhs);
-    friend std::ostream& operator<<(std::ostream& stream, const sql& s);
-    friend sql operator%(const sql&, const char* str);
-    friend sql operator%(const char* str, const sql&);
-};
-
-bool operator==(const sql& lhs, const sql& rhs){
-    return lhs.query() == rhs.query();
-}
-
-sql operator%(const sql&, const char* str){
-    return sql(str);
-}
-
-sql operator%(const char* str, const sql&){
-    return sql(str);
-}
-
-std::ostream& operator<<(std::ostream& stream, const sql& s){
-    stream << s.query();
-    return stream;
-}
-
-#define SQL_EXPECT_SAME(x, y) CHECK(sql() % x.text().c_str() == y % sql())
-
 
 TEST_CASE("postgresql crud join", "[pg]"){
     boost::asio::io_service io;
@@ -175,9 +76,9 @@ TEST_CASE("postgresql crud join", "[pg]"){
         void
     >;
     CHECK(std::is_same<basic_simple_join_1_t::data_of<articles::title>, articles::table::column<articles::title>>::value);
-    CHECK(std::is_same<basic_simple_join_1_t::data_of<students::name>,  students::table::column<students::name>>::value);
+    CHECK(std::is_same<basic_simple_join_1_t::data_of<students::first_name>,  students::table::column<students::first_name>>::value);
     CHECK(std::is_same<basic_simple_join_1_t::relation_of<articles::title>, articles::table>::value);
-    CHECK(std::is_same<basic_simple_join_1_t::relation_of<students::name>,  students::table>::value);
+    CHECK(std::is_same<basic_simple_join_1_t::relation_of<students::first_name>,  students::table>::value);
     CHECK(std::is_same<basic_simple_join_1_t::source, articles::table>::value);
     using simple_join_1_t = basic_simple_join_1_t::fetch::all::apply;
     auto simple_join_1_t_collector = udho::activities::collect<simple_join_1_t>(ctx);
@@ -188,9 +89,13 @@ TEST_CASE("postgresql crud join", "[pg]"){
             articles.title,                     \
             articles.author,                    \
             articles.project,                   \
+            articles.published,                 \
+            articles.content,                   \
             students.id,                        \
-            students.name,                      \
-            students.marks                      \
+            students.first_name,                \
+            students.last_name,                 \
+            students.marks,                     \
+            students.age                        \
         from articles                           \
         inner join students                     \
             on articles.author = students.id    \
@@ -232,10 +137,10 @@ TEST_CASE("postgresql crud join", "[pg]"){
         >::type
     >;
     CHECK(std::is_same<basic_simple_join_2_t::data_of<articles::title>, articles::table::column<articles::title>>::value);
-    CHECK(std::is_same<basic_simple_join_2_t::data_of<students::name>,  students::table::column<students::name>>::value);
+    CHECK(std::is_same<basic_simple_join_2_t::data_of<students::first_name>,  students::table::column<students::first_name>>::value);
     CHECK(std::is_same<basic_simple_join_2_t::data_of<projects::title>, projects::table::column<projects::title>>::value);
     CHECK(std::is_same<basic_simple_join_2_t::relation_of<articles::title>, articles::table>::value);
-    CHECK(std::is_same<basic_simple_join_2_t::relation_of<students::name>,  students::table>::value);
+    CHECK(std::is_same<basic_simple_join_2_t::relation_of<students::first_name>,  students::table>::value);
     CHECK(std::is_same<basic_simple_join_2_t::relation_of<projects::title>, projects::table>::value);
     CHECK(std::is_same<basic_simple_join_2_t::source, articles::table>::value);
     using simple_join_2_t = basic_simple_join_2_t::fetch::all::apply;
@@ -247,12 +152,17 @@ TEST_CASE("postgresql crud join", "[pg]"){
             articles.title,                       \
             articles.author,                      \
             articles.project,                     \
+            articles.published,                   \
+            articles.content,                     \
             projects.id,                          \
             projects.title,                       \
+            projects.started,                     \
             projects.admin,                       \
             students.id,                          \
-            students.name,                        \
-            students.marks                        \
+            students.first_name,                  \
+            students.last_name,                   \
+            students.marks,                       \
+            students.age                          \
         from articles                             \
         inner join projects                       \
             on articles.project = projects.id     \
@@ -321,10 +231,10 @@ TEST_CASE("postgresql crud join", "[pg]"){
         >::type
     >;
     CHECK(std::is_same<basic_simple_join_2a_t::data_of<articles::title>, articles::table::column<articles::title>>::value);
-    CHECK(std::is_same<basic_simple_join_2a_t::data_of<students::name>,  students::table::column<students::name>>::value);
+    CHECK(std::is_same<basic_simple_join_2a_t::data_of<students::first_name>,  students::table::column<students::first_name>>::value);
     CHECK(std::is_same<basic_simple_join_2a_t::data_of<projects::title>, projects::table::column<projects::title>>::value);
     CHECK(std::is_same<basic_simple_join_2a_t::relation_of<articles::title>, articles::table>::value);
-    CHECK(std::is_same<basic_simple_join_2a_t::relation_of<students::name>,  students::table>::value);
+    CHECK(std::is_same<basic_simple_join_2a_t::relation_of<students::first_name>,  students::table>::value);
     CHECK(std::is_same<basic_simple_join_2a_t::relation_of<projects::title>, projects::table>::value);
     CHECK(std::is_same<basic_simple_join_2a_t::source, articles::table>::value);
     using simple_join_2a_t = basic_simple_join_2a_t::fetch::all::apply;
@@ -336,12 +246,17 @@ TEST_CASE("postgresql crud join", "[pg]"){
             articles.title,                       \
             articles.author,                      \
             articles.project,                     \
+            articles.published,                   \
+            articles.content,                     \
             projects.id,                          \
             projects.title,                       \
+            projects.started,                     \
             projects.admin,                       \
             students.id,                          \
-            students.name,                        \
-            students.marks                        \
+            students.first_name,                  \
+            students.last_name,                   \
+            students.marks,                       \
+            students.age                          \
         from articles                             \
         inner join projects                       \
             on articles.project = projects.id     \
@@ -411,11 +326,15 @@ TEST_CASE("postgresql crud join", "[pg]"){
             articles.title,                       \
             articles.author,                      \
             articles.project,                     \
+            articles.published,                   \
+            articles.content,                     \
             projects.id,                          \
             projects.title,                       \
+            projects.started,                     \
             projects.admin,                       \
             projects.id,                          \
             projects.title,                       \
+            projects.started,                     \
             projects.admin                        \
         from articles                             \
         inner join projects                       \
@@ -448,11 +367,11 @@ TEST_CASE("postgresql crud join", "[pg]"){
         >::type
     >;
     CHECK(std::is_same<basic_simple_join_3_t::data_of<articles::title>, articles::table::column<articles::title>>::value);
-    CHECK(std::is_same<basic_simple_join_3_t::data_of<students::name>,  students::table::column<students::name>>::value);
+    CHECK(std::is_same<basic_simple_join_3_t::data_of<students::first_name>,  students::table::column<students::first_name>>::value);
     CHECK(std::is_same<basic_simple_join_3_t::data_of<projects::title>, projects::table::column<projects::title>>::value);
     CHECK(std::is_same<basic_simple_join_3_t::data_of<memberships::id>, memberships::table::column<memberships::id>>::value);
     CHECK(std::is_same<basic_simple_join_3_t::relation_of<articles::title>, articles::table>::value);
-    CHECK(std::is_same<basic_simple_join_3_t::relation_of<students::name>,  students::table>::value);
+    CHECK(std::is_same<basic_simple_join_3_t::relation_of<students::first_name>,  students::table>::value);
     CHECK(std::is_same<basic_simple_join_3_t::relation_of<projects::title>, projects::table>::value);
     CHECK(std::is_same<basic_simple_join_3_t::relation_of<memberships::id>, memberships::table>::value);
     CHECK(std::is_same<basic_simple_join_3_t::source, articles::table>::value);
@@ -465,14 +384,19 @@ TEST_CASE("postgresql crud join", "[pg]"){
             articles.title,                            \
             articles.author,                           \
             articles.project,                          \
+            articles.published,                        \
+            articles.content,                          \
             students.id,                               \
-            students.name,                             \
+            students.first_name,                       \
+            students.last_name,                        \
             students.marks,                            \
+            students.age,                              \
             memberships.id,                            \
             memberships.student,                       \
             memberships.project,                       \
             projects.id,                               \
             projects.title,                            \
+            projects.started,                          \
             projects.admin                             \
         from articles                                  \
         inner join students                            \
--- tests2/db/pg/model.cpp
+++ tests2/db/pg/model.cpp
@@ -8,6 +8,7 @@
 #include <tuple>
 #include <regex>
 
+#include "schema.h"
 #include "common.h"
 
 #include <udho/db/pg/activities/basic.h>
@@ -36,71 +37,6 @@
 namespace db = udho::db;
 namespace pg = db::pg;
 
-// { relations
-
-namespace students{
-
-    PG_ELEMENT(id,          pg::types::bigserial, pg::constraints::primary);
-    PG_ELEMENT(first_name,  pg::types::varchar, pg::constraints::not_null);
-    PG_ELEMENT(last_name,   pg::types::varchar);
-    PG_ELEMENT(marks,       pg::types::integer);
-    PG_ELEMENT(age,         pg::types::integer);
-    PG_ELEMENT(writer,      pg::types::bigint);
-    PG_ELEMENT(name,        pg::types::text);
-
-    struct table: pg::relation<table, id, first_name, last_name, marks, age>{
-        PG_NAME(students)
-        using readonly = pg::readonly<id>;
-    };
-
-}
-
-namespace articles{
-
-    PG_ELEMENT(id,        pg::types::bigserial, pg::constraints::primary);
-    PG_ELEMENT(title,     pg::types::varchar,   pg::constraints::unique);
-    PG_ELEMENT(author,    pg::types::bigint,    pg::constraints::references<students::table::column<students::id>>::restrict);
-    PG_ELEMENT(project,   pg::types::bigint);
-    PG_ELEMENT(published, pg::types::timestamp, pg::constraints::not_null, pg::constraints::default_<pg::constants::now>::value);
-    PG_ELEMENT(content,   pg::types::text);
-
-    struct table: pg::relation<table, id, title, author, project, published, content>{
-        PG_NAME(articles)
-        using readonly = pg::readonly<id>;
-    };
-
-}
-
-namespace projects{
-
-    PG_ELEMENT(id,          pg::types::bigserial, pg::constraints::primary);
-    PG_ELEMENT(title,       pg::types::varchar);
-    PG_ELEMENT(started,     pg::types::timestamp, pg::constraints::not_null, pg::constraints::default_<pg::constants::now>::value);
-    PG_ELEMENT(admin,       pg::types::bigint);
-
-    struct table: pg::relation<table, id, title, started, admin>{
-        PG_NAME(projects)
-        using readonly = pg::readonly<id>;
-    };
-
-}
-
-namespace memberships{
-
-    PG_ELEMENT(id,      pg::types::bigserial, pg::constraints::primary);
-    PG_ELEMENT(student, pg::types::bigint, pg::constraints::references<students::table::column<students::id>>::restrict);
-    PG_ELEMENT(project, pg::types::bigint, pg::constraints::references<projects::table::column<projects::id>>::restrict);
-
-    struct table: pg::relation<table, id, student, project>{
-        PG_NAME(memberships)
-        using readonly = pg::readonly<id>;
-    };          
-
-}
-
-// } relations
-
-
 boost::asio::io_service io;
 udho::servers::quiet::stateless::request_type req;
 udho::servers::quiet::stateless::attachment_type attachment(io);
@@ -121,10 +57,10 @@ TEST_CASE("postgresql CREATE query", "[pg]") {
     SQL_EXPECT_SAME(
         create_articles_after_skip(create_articles_after_skip_collector, pool, io).sql(),
         "create table if not exists articles(                               \
-            id bigserial primary key,                                          \
+            id bigserial primary key,                                       \
             title varchar unique,                                           \
             author bigint references students(id) on delete restrict,       \
-            project bigint,                                                 \
+            project bigint references projects(id) on delete restrict,      \
             published timestamp without time zone not null default now(),   \
             content text                                                    \
         )"
@@ -141,10 +77,10 @@ TEST_CASE("postgresql CREATE query", "[pg]") {
     SQL_EXPECT_SAME(
         create_articles_except_after_skip(create_articles_except_after_skip_collector, pool, io).sql(),
         "create table if not exists articles(                               \
-            id bigserial primary key,                                          \
+            id bigserial primary key,                                       \
             title varchar unique,                                           \
             author bigint references students(id) on delete restrict,       \
-            project bigint,                                                 \
+            project bigint references projects(id) on delete restrict,      \
             published timestamp without time zone not null default now()    \
         )"
     );
@@ -160,10 +96,10 @@ TEST_CASE("postgresql CREATE query", "[pg]") {
     SQL_EXPECT_SAME(
         create_articles_only_after_skip(create_articles_only_after_skip_collector, pool, io).sql(),
         "create table if not exists articles(                               \
-            id bigserial primary key,                                          \
+            id bigserial primary key,                                       \
             title varchar unique,                                           \
             author bigint references students(id) on delete restrict,       \
-            project bigint,                                                 \
+            project bigint references projects(id) on delete restrict,      \
             published timestamp without time zone not null default now()    \
         )"
     );
