b928b4e5e256b10527c3d90cb6dea94ec819c462
neel.basu <neel.basu.z@gmail.com>
2023-06-23 01:55:43 +0200

    - encapsulate function

--- includes/udho/hazo/detail/function.h
+++ includes/udho/hazo/detail/function.h
@@ -34,11 +34,50 @@
 #include <udho/hazo/detail/indices.h>
 #include <udho/hazo/detail/extraction_helper.h>
 #include <udho/hazo/node/fwd.h>
+#include <boost/lexical_cast.hpp>
 
 namespace udho{
 namespace hazo{
 
 namespace detail{
+
+    template <typename T, int Index>
+    struct cast_optionally{
+        template <typename IteratorT>
+        static T cast(IteratorT begin){
+            std::string argstr(*(begin + Index));
+            try{
+                return boost::lexical_cast<T>(argstr);
+            }catch(...){
+                throw std::invalid_argument("failed to cast "+argstr);
+                return T();
+            }
+        }
+    };
+
+    template <typename TupleT, int Index=std::tuple_size<TupleT>::value-1>
+    struct arg_to_tuple: arg_to_tuple<TupleT, Index-1>{
+        typedef arg_to_tuple<TupleT, Index-1> base_type;
+
+        template <typename IteratorT>
+        static void convert(TupleT& tuple, IteratorT begin){
+            std::get<Index>(tuple) = cast_optionally<typename std::tuple_element<Index, TupleT>::type, Index>::cast(begin);
+            base_type::convert(tuple, begin);
+        }
+    };
+
+    template <typename TupleT>
+    struct arg_to_tuple<TupleT, 0>{
+        template <typename IteratorT>
+        static void convert(TupleT& tuple, IteratorT begin){
+            std::get<0>(tuple) = cast_optionally<typename std::tuple_element<0, TupleT>::type, 0>::cast(begin);
+        }
+    };
+
+    template <typename TupleT, typename IteratorT>
+    void arguments_to_tuple(TupleT& tuple, IteratorT begin){
+        arg_to_tuple<TupleT>::convert(tuple, begin);
+    }
 
     /**
      * extract the function signature
@@ -48,30 +87,91 @@ namespace detail{
 
     template <typename R, typename... Args>
     struct function_signature_<R (*)(Args...)>{
-        typedef R                     return_type;
-        typedef void                  object_type;
-        typedef std::tuple<Args...>   arguments_type;
+        typedef R                                 return_type;
+        typedef void                              object_type;
+        typedef std::tuple<Args...>               arguments_type;
+        typedef std::tuple<std::decay_t<Args>...> decayed_arguments_type;
     };
 
     template <typename R, typename... Args>
     struct function_signature_<R (Args...)>{
-        typedef R                     return_type;
-        typedef void                  object_type;
-        typedef std::tuple<Args...>   arguments_type;
+        typedef R                                 return_type;
+        typedef void                              object_type;
+        typedef std::tuple<Args...>               arguments_type;
+        typedef std::tuple<std::decay_t<Args>...> decayed_arguments_type;
     };
 
     template <typename R, typename C, typename... Args>
     struct function_signature_<R (C::*)(Args...)>{
-        typedef R                     return_type;
-        typedef C                     object_type;
-        typedef std::tuple<Args...>   arguments_type;
+        typedef R                                 return_type;
+        typedef std::tuple<Args...>               arguments_type;
+        typedef std::tuple<std::decay_t<Args>...> decayed_arguments_type;
+        typedef C                                 object_type;
     };
 
     template <typename R, typename C, typename... Args>
     struct function_signature_<R (C::*)(Args...) const>{
-        typedef R                     return_type;
-        typedef const C               object_type;
-        typedef std::tuple<Args...>   arguments_type;
+        typedef R                                 return_type;
+        typedef std::tuple<Args...>               arguments_type;
+        typedef std::tuple<std::decay_t<Args>...> decayed_arguments_type;
+        typedef const C                           object_type;
+    };
+
+    template <typename F>
+    struct encapsulate_mem_function{
+        using signature              = function_signature_<F>;
+        using return_type            = typename signature::return_type;
+        using arguments_type         = typename signature::arguments_type;
+        using object_type            = typename signature::object_type;
+        using decayed_arguments_type = typename signature::decayed_arguments_type;
+
+        encapsulate_mem_function(F f, object_type* that): _f(f), _that(that) {}
+        return_type operator()(decayed_arguments_type&& args){
+            return std::apply(_f, std::tuple_cat(std::make_tuple(_that), args));
+        }
+        return_type operator()(arguments_type&& args){
+            return std::apply(_f, std::tuple_cat(std::make_tuple(_that), args));
+        }
+        template <typename IteratorT>
+        return_type operator()(IteratorT begin, IteratorT end){
+            decayed_arguments_type decayed_args;
+            auto nargs = std::distance(begin, end);
+            if(nargs != std::tuple_size_v<decayed_arguments_type>()){
+                throw std::out_of_range("expects " + std::to_string(std::tuple_size_v<decayed_arguments_type>()) + " args but called with " + nargs + " arguments");
+            }
+            arguments_to_tuple(decayed_args, begin);
+            return operator()(decayed_args);
+        }
+        private:
+            F _f;
+            object_type* _that;
+    };
+    template <typename F>
+    struct encapsulate_function{
+        using signature              = function_signature_<F>;
+        using return_type            = typename signature::return_type;
+        using arguments_type         = typename signature::arguments_type;
+        using decayed_arguments_type = typename signature::decayed_arguments_type;
+
+        encapsulate_function(F f): _f(f) {}
+        return_type operator()(decayed_arguments_type&& args){
+            return std::apply(_f, args);
+        }
+        return_type operator()(arguments_type&& args){
+            return std::apply(_f, args);
+        }
+        template <typename IteratorT>
+        return_type operator()(IteratorT begin, IteratorT end){
+            decayed_arguments_type decayed_args;
+            auto nargs = std::distance(begin, end);
+            if(nargs != std::tuple_size<decayed_arguments_type>::value){
+                throw std::out_of_range("expects " + std::to_string(std::tuple_size<decayed_arguments_type>::value) + " args but called with " + std::to_string(nargs) + " arguments");
+            }
+            arguments_to_tuple(decayed_args, begin);
+            return operator()(decayed_args);
+        }
+        private:
+            F _f;
     };
 
     template <typename F>
--- sandbox.cpp
+++ sandbox.cpp
@@ -1,7 +1,78 @@
 #include <iostream>
+#include <typeinfo>
+#include <udho/hazo/detail/function.h>
 
+void f0(int a, const std::string& b, const double& c, bool d){
+    return;
+}
+
+int f1(int a, const std::string& b, const double& c, bool d){
+    std::cout << a << " " << b << " " << c << " " << d << std::endl;
+    return 42;
+}
+
+std::string f2(int a, const std::string& b, const double& c, bool d){
+    return "42";
+}
+
+struct X{
+    void f0(int a, const std::string& b, const double& c, bool d){
+        return;
+    }
+
+    int f1(int a, const std::string& b, const double& c, bool d){
+        return 42;
+    }
+
+    std::string f2(int a, const std::string& b, const double& c, bool d){
+        return "42";
+    }
+
+    int f3(int a, const std::string& b, const double& c, bool d) const{
+        return 84;
+    }
+};
 
 int main(){
-    
+    static_assert(std::is_same_v<decltype(udho::hazo::detail::function_signature( f0))::return_type, void>);
+    static_assert(std::is_same_v<decltype(udho::hazo::detail::function_signature(&f0))::return_type, void>);
+    static_assert(std::is_same_v<decltype(udho::hazo::detail::function_signature( f1))::return_type, int>);
+    static_assert(std::is_same_v<decltype(udho::hazo::detail::function_signature(&f1))::return_type, int>);
+    static_assert(std::is_same_v<decltype(udho::hazo::detail::function_signature( f2))::return_type, std::string>);
+    static_assert(std::is_same_v<decltype(udho::hazo::detail::function_signature(&f2))::return_type, std::string>);
+
+    static_assert(std::is_same_v<decltype(udho::hazo::detail::function_signature( f0))::arguments_type, std::tuple<int, const std::string&, const double&, bool>>);
+    static_assert(std::is_same_v<decltype(udho::hazo::detail::function_signature(&f0))::arguments_type, std::tuple<int, const std::string&, const double&, bool>>);
+    static_assert(std::is_same_v<decltype(udho::hazo::detail::function_signature( f1))::arguments_type, std::tuple<int, const std::string&, const double&, bool>>);
+    static_assert(std::is_same_v<decltype(udho::hazo::detail::function_signature(&f1))::arguments_type, std::tuple<int, const std::string&, const double&, bool>>);
+    static_assert(std::is_same_v<decltype(udho::hazo::detail::function_signature( f2))::arguments_type, std::tuple<int, const std::string&, const double&, bool>>);
+    static_assert(std::is_same_v<decltype(udho::hazo::detail::function_signature(&f2))::arguments_type, std::tuple<int, const std::string&, const double&, bool>>);
+
+    std::string str = "hello";
+    double d = 2.4;
+
+    {
+        std::vector<std::string> args;
+        args.push_back("24");
+        args.push_back("world");
+        args.push_back("2.42");
+        args.push_back("0");
+
+        auto f = udho::hazo::detail::encapsulate_function(f1);
+        // std::cout << f(decltype(f)::decayed_arguments_type()) << std::endl;
+        // std::cout << f(decltype(f)::arguments_type(0, str, d, false)) << std::endl;
+
+        f(args.begin(), args.end());
+    }
+
+    // X x;
+    // const X& y=x;
+    //
+    // {
+    //     auto f = udho::hazo::detail::encapsulate_mem_function(&X::f3, &y);
+    //     std::cout << f(decltype(f)::decayed_arguments_type()) << std::endl;
+    //     std::cout << f(decltype(f)::arguments_type(0, str, d, false)) << std::endl;
+    // }
+
     return 0;
 }
